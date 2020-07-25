f947f06d2b6421105c46c4d8c0426565e33ea883
nazmul hossain <nazmuljuit@gmail.com>
2020-07-25 16:55:40 +0600

    inline style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-//import axios from 'axios';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
+let paragraph = "Welcome to React App Development";
+let name ="Ryan";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome to ottean software solutions
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>{ paragraph } { name }</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import './index.css';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// a variable declared with const can not be changed
+// a variable declared with let can be changed
+
+// but if a variable is an array or object
+// its value can get altered
+
+const indentity = {
+    name: "My Name is Nazmul Hossain",
+    address: "I am from Bangladesh"
+};
+
+class Hello extends React.Component {
+    render(){
+
+        indentity.iAmNewPara = "I am a programmer. I am a Masters in IT !";
+
+        return(
+
+            <div>
+
+                <h1 style={{color:"green"}}>{indentity.name}</h1>
+                <h3>{indentity.address}</h3>
+                <p style={{background:"gray"}}>{indentity.iAmNewPara}</p>
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+registerServiceWorker();
