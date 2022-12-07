7601e751e99e317a696efa26c40b5f67d853f6ae
Nazmul Hossain <nazmul.hossain@shampanit.com>
2022-12-07 18:59:10 +0600

    working with jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const wlcome = 'Welcome to first react app!';
+const reactVersion = 'React Version 18.2.0';
+const reactDomV = 'React Dom Version 18.2.0';
 root.render(
-
-    <h1>Welcome to first react app! </h1>
+  <div>
+    <h1>{wlcome}</h1>
+    <h3>{reactVersion}</h3>
+    <h5>{reactDomV}</h5>
+  </div>
   
 );
 
