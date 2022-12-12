02592a99e2f791553475c9fc9a01944b35a088b1
Nazmul Hossain <nazmul@watteen.com>
2022-12-12 19:16:19 +0600

    update some css style

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const wlcome = 'Welcome to first react app!';
 const reactVersion = 'React Version 18.2.0';
 const reactDomV = 'React Dom Version 18.2.0';
+const cardTitle = 'My Card';
+const cardDescriptions = 'It is my card. One of the best card....';
 root.render(
   <div>
     <h1>{wlcome}</h1>
     <h3>{reactVersion}</h3>
     <h5 className='headerFive'>{reactDomV}</h5>
+
+    <div className='card'>
+      <h2 className='cardTItle'>{cardTitle}</h2>
+      <p className='cardDescriptions'>{cardDescriptions}</p>
+
+    </div>
   </div>
   
 );
