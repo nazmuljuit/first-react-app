12d7d1e665c78b4c29a4f265b68e3ba8a22913b6
Nazmul Hossain <nazmul@watteen.com>
2022-12-27 15:11:57 +0600

    mapping data to components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./index.css";
 import Card from "./components/Card";
+import Data from "./data.json";
 
 const wlcome = 'Welcome to First React App!';
 const reactVersion = 'React Version 18.2.0';
@@ -8,16 +9,21 @@ const reactDomV = 'React Dom Version 18.2.0';
 
 function App()
 {
+    let items = [];
+    /*for(let x=0;x< Data.length;x++){
+    	items.push(<Card cardTitle={Data[x].title} cardDescriptions={Data[x].description} />)
+    }*/
 
+    items = Data.map((item,index) => <Card key={index} cardTitle={item.title} cardDescriptions={item.description} />);
     return <div>
         <div className="headingDiv">
             <h1>{wlcome}</h1>
             <h3>{reactVersion}</h3>
             <h5 className='headerFive'>{reactDomV}</h5>
         </div>
-    <Card cardTitle="My First Card" cardDescriptions="It is my card. One of the best card...."/>
-    <Card cardTitle="My Second Card" cardDescriptions="It is my card. One of the best card...."/>
-    <Card cardTitle="My Third Card" cardDescriptions="It is my card. One of the best card...."/>
+        {items}
+    
+
 
     </div>
 
